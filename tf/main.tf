c57769a4731d55059579befac2502fb6e1df65a3
sximada <50688746+sximada@users.noreply.github.com>
2020-01-05 12:53:55 +0900

    Add postgres settings

--- tf/main.tf
+++ tf/main.tf
@@ -7,3 +7,15 @@ provider "mysql" {
 resource "mysql_database" "sbk" {
   name = "sbk_local"
 }
+
+provider "postgresql" {
+  host = "127.0.0.1"
+  username = "postgres"
+  password = ""
+  sslmode  = "disable"
+}
+
+resource "postgresql_database" "inv" {
+  provider = "postgresql"
+  name = "inv_local"
+}
